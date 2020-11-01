4048fbeceae577db1b40c864d25ff7ebe5f693f8
Gal <galaharon12321@gmail.com>
2020-11-01 03:27:34 +0200

    Learning

---
+++ Lesson 1/targil1.py
@@ -0,0 +1,27 @@
+
+a=12
+b=11.56
+c="shalom!"
+
+# This is Integer
+print(a)
+print(type (a))
+
+
+# This is Float
+print(b)
+print(type(b))
+
+# This is String
+print (c)
+print(type(c))
+
+
+
+name=input("Enter your name: ")
+age=input("Enter your age: ")
+mail=input("Enter you mail: ")
+
+
+print("\nFull name " + name + "\nYour age is: " + str(age) + " \nYour mail: " + mail)
+
